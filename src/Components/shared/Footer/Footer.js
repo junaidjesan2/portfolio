35a4f9e11d46af961b77688191acf80b08a44520
junaidjesan2 <junaidjesan2@gmail.com>
2024-03-19 15:13:03 +0600

    footer pedding decreased and set alignment

--- src/Components/shared/Footer/Footer.js
+++ src/Components/shared/Footer/Footer.js
@@ -3,29 +3,27 @@ import { FiFacebook, FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className="footer justify-evenly py-16 border-t-2">
+    <footer className="footer justify-evenly py-6 border-t-2">
       <div>
-        <ul className="grid grid-flow-col gap-4">
+        <ul className="grid grid-flow-col gap-7">
           <li>
             <a href="https://github.com/junaidjesan2">
               <FiGithub className="h-7 w-7" />
-              GitHub
             </a>
           </li>
           <li>
             <a href="https://www.linkedin.com/in/junaidjesan2/">
               <FiLinkedin className="h-7 w-7" />
-              LinkedIn
             </a>
           </li>
           <li>
             <a href="https://www.facebook.com/junaidjesan2">
-              <FiFacebook className="h-7 w-7" /> Facebook
+              <FiFacebook className="h-7 w-7" />
             </a>
           </li>
           <li>
             <a href="https://www.instagram.com/junaidjesan2">
-              <FiInstagram className="h-7 w-7" /> Instagram
+              <FiInstagram className="h-7 w-7" />
             </a>
           </li>
         </ul>
