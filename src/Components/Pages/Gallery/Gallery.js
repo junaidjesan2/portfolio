3c6df9105ed4a8cf522aa38fa54a76c407b01365
junaidjesan2 <junaidjesan2@gmail.com>
2024-03-20 13:43:55 +0600

    single image view through modal functionality added

--- src/Components/Pages/Gallery/Gallery.js
+++ src/Components/Pages/Gallery/Gallery.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import ProjectImage from "../../../data/ProjectsImage";
 import toast from "react-hot-toast";
 
+import { CgCloseR } from "react-icons/cg";
+
 export default function Galley() {
-  const notify=()=>toast("Work in progress. Stay with me for better experience",{duration:1500})
+  const notify = () =>
+    toast("Work in progress. Stay with me for better experience", {
+      duration: 1500,
+    });
   return (
     <div>
       <div className="sticky top-3 py-5">
@@ -17,7 +22,7 @@ export default function Galley() {
         <ResponsiveMasonry columnsCountBreakPoints={{ 300: 1, 500: 2, 900: 3 }}>
           <Masonry columnsCount={3} gutter="10px">
             {ProjectImage.map((image) => (
-              <img key={image.id} src={image.src} />
+              <ShowImage image={image} key={image.id}></ShowImage>
             ))}
           </Masonry>
         </ResponsiveMasonry>
@@ -32,5 +37,28 @@ export default function Galley() {
       </div>
     </div>
   );
+}
 
+const ShowImage = (image) => {
+  console.log(image.image);
+  const [openImage, setOpenImage] = useState(false);
+  return (
+    <div className="">
+      <img
+        key={image.id}
+        src={image.image.src}
+        onClick={() => setOpenImage(true)}
+      />
+      {openImage && (
+        <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+          <div className="bg-white p-2 h-screen rounded mx-28 my-16">
+          <CgCloseR onClick={()=>setOpenImage(false)} className= "fixed h-6 w-6 text-black"/>
+          <div className="h-screen overflow-y-scroll">
+            <img src={image.image.src} className=""/>
+          </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
