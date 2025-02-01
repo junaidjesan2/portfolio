b0781ad3b29939854a7bda514754fdde31472c2d
junaidjesan2 <junaidjesan2@gmail.com>
2025-02-01 23:44:57 +0600

    section left side design

--- src/Components/shared/Section/Section.js
+++ src/Components/shared/Section/Section.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 // Styled Paragraph Component
 const Section = ({ children }) => {
-    const paragraphStyle = "md:text-3xl text-[#be94f9] border-l-cyan-400 border-l-4 pl-4 text-xl flex gap-3 items-center text-start font-semibold"
+    const paragraphStyle = "md:text-3xl text-[#be94f9] border-l-cyan-400 border-l-4 rounded-l-md pl-4 text-xl flex gap-3 items-center text-start font-semibold"
 
     return <>
-        <div data-aos="fade-down"
+        <div data-aos="fade-right"
             data-aos-easing="linear"
             data-aos-duration="1500"
             className="py-3">
